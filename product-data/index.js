531c68b7d60ad9a99858db22b13acb9cfe9d5f2e
rohit0985 <rohitsharma4681@gmail.com>
2023-04-01 23:30:20 +0530

    scrapped and added some data from original website

---
+++ product-data/index.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const data = fs.readFileSync("./edit.json", "utf-8");
+const parsedData = JSON.parse(data);
+const updated = [];
+
+const s = ["XS","S","M","L","XL","2XL","3XL"];
+// const s = ['30','32','34','36','38','40','42','44','46'];
+
+
+for (let i = 0; i < parsedData.length; i++) {
+    let el = parsedData[i]
+  let obj = {
+    image: el.i,
+    brand: el.b,
+    color : "Pink",
+    title: el.t,
+    size: [],
+    sellingPrice: + el.sp,
+    marketPrice: + el.mp,
+    percentageDiscount: el.d.includes("%") ?  el.d : "",
+    faltDiscount: el.d.includes("%") ? "" : el.d,
+    inventory: 10,
+  };
+  if (obj.percentageDiscount) {
+    let x = obj.percentageDiscount.split("");
+    let y = x.slice(1, x.indexOf("%"));
+    obj.percentageDiscount = Number(y.join(""));
+  }
+
+  if (obj.faltDiscount) {
+    let x = obj.faltDiscount.replace("(","").replace(")","").split(" ");
+    obj.faltDiscount = Number(x[0]);
+  }
+
+
+if(!s.includes(obj.size)) s.push(obj.size)
+
+while(obj.size.length<4){
+  let idx = Math.floor(Math.random()*7)
+ 
+if(!obj.size.includes(s[idx])) obj.size.push(s[idx])
+// obj.size = obj.size.sort((a,b)=> Number(a)-Number(b))
+obj.size = obj.size.sort((a,b)=> s.indexOf(a)-s.indexOf(b))
+}
+
+
+
+
+
+obj.collections = "Women"
+obj.subCollection = "Fusion Wear"
+obj.gender = "Women"
+obj.category = "Jackets"
+  updated.push(obj);
+
+}
+// console.log(s)
+
+fs.writeFileSync("./update.json", JSON.stringify(updated), "utf-8");
---
+++ product-data/random.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const data = fs.readFileSync("./edit.json", "utf-8");
+let parseddata = JSON.parse(data);
+const suffeled = [];
+
+let parsedData = parseddata.filter((el,i)=> el.sellingPrice !== 0 && typeof(el.sellingPrice !== "string"))
+
+while (parsedData.length > 0) {
+  const ri = Math.floor(Math.random() * parsedData.length);
+  const ro = parsedData[ri];
+  if(parsedData.sellingPrice && !parsedData.marketPrice){
+    parsedData.marketPrice = parsedData.sellingPrice
+    parsedData.percentageDiscount = ""
+   parsedData.faltDiscount = ""
+  }
+  suffeled.push(ro);
+  parsedData.splice(ri, 1);
+}
+
+fs.writeFileSync("./update.json", JSON.stringify(suffeled), "utf-8");
